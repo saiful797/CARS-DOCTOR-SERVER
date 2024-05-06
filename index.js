d4a006ac3ffb59e27b21b39e9346773f2ef506d3
saiful797 <saifulislamsaif797@gmail.com>
2024-05-06 17:54:05 +0600

    Car doctor server connect with mongodb successfuly

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${user}:${password}@cluster0.a3qxp45.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const servicesCollection = client.db('CarDoctorDB').collection('serveces');
+
+    app.get('/services', async(req, res) => {
+        const result = await servicesCollection.find().toArray();
+        res.send(result);
+
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req, res)=>{
+    res.send('Car doctor server is running...');
+})
+app.listen(port, ()=>{
+    console.log(`Car doctor server is running on port ${port}`);
+})
